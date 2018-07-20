3561dd39ea7a855b251a24d9d5b887f8ff02ae08
Marat <marat.curious@gmail.com>
2018-07-20 17:27:38 +0300

    add shop and category components to router

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,8 @@ import {
   Route,
   Link
 } from 'react-router-dom';
-
-
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-)
-
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-)
+import Shop from './Shop';
+import Category from './Category';
 
 const Topic = ({ match }) => (
   <div>
@@ -56,15 +45,15 @@ const App = () => (
   <Router>
     <div>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
+        <li><Link to="/shop">Shop</Link></li>
+        <li><Link to="/category">Category</Link></li>
         <li><Link to="/topics">Topics</Link></li>
       </ul>
 
       <hr/>
 
-      <Route exact path="/" component={Home}/>
-      <Route path="/about" component={About}/>
+      <Route exact path="/shop" component={Shop}/>
+      <Route path="/category" component={Category}/>
       <Route path="/topics" component={Topics}/>
     </div>
   </Router>
