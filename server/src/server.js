c1bad27504c2ba5c6b449d76f670decfe049e02a
Marat <marat.curious@gmail.com>
2018-07-25 12:28:56 +0300

    add CORS rules

--- server/src/server.js
+++ server/src/server.js
@@ -8,6 +8,12 @@ import router from './routes';
 let server = express();
 let port = process.env.PORT || 8000;
 
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(bodyParser.json());
 server.use('/api', router);
