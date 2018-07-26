eb70309e3c1ea25989c331453497d96a72197a33
Marat <marat.curious@gmail.com>
2018-07-26 11:51:29 +0300

    styles for list item

--- client/src/components/ShopList.js
+++ client/src/components/ShopList.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import Edit from '../assets/icons/edit.svg';
+import Delete from '../assets/icons/delete.svg';
 import '../styles/list.css';
 
 function ShopItem(props) {
   const items = props.items.map((item) =>
-    <li key={item._id}>{item.name}</li>
+    <li
+      key={item._id}
+      className="list__item"
+    >
+      <span className="list__item-name">{item.name}</span>
+      <img
+        src={Edit}
+        className="list__item-icon list__item-icon_edit"
+      />
+      <img
+        src={Delete}
+        className="list__item-icon list__item-icon_delete"
+      />
+    </li>
   );
   return <ul>{items}</ul>;
 };
