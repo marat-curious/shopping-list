449a7e65edb63cbbfd2bdcc1acfa9a11bc8bf675
Marat <marat.curious@gmail.com>
2018-07-27 13:21:46 +0300

    add buttons to actions with list item

--- client/src/components/ShopList.js
+++ client/src/components/ShopList.js
@@ -1,24 +1,37 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import Header from './Header';
 import Edit from '../assets/icons/edit.svg';
 import Delete from '../assets/icons/delete.svg';
 import '../styles/page.css';
 import '../styles/list.css';
 
-function ShopItem(props) {
+const Button = withRouter(({ history, type }) => {
+  return (
+    <button
+      className={ `list__button list__button_${type}` }
+      onClick={ () => { history.push(`/shop/${type}`) }}
+    >
+      <img
+        src={ type === 'edit' ? Edit : Delete }
+        className={ `list__item-icon list__icon_${type}` }
+      />
+    </button>
+  );
+});
+
+const ShopItem = props => {
   const items = props.items.map((item) =>
     <li
       key={item._id}
       className="list__item"
     >
       <span className="list__item-name">{item.name}</span>
-      <img
-        src={Edit}
-        className="list__item-icon list__item-icon_edit"
+      <Button
+        type="edit"
       />
-      <img
-        src={Delete}
-        className="list__item-icon list__item-icon_delete"
+      <Button
+        type="delete"
       />
     </li>
   );
