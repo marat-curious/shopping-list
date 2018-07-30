ff715c7d1bec3b073e41303045bddfe12ea0045f
Marat <marat.curious@gmail.com>
2018-07-30 12:57:42 +0300

    add CORS methods

--- server/src/server.js
+++ server/src/server.js
@@ -11,6 +11,7 @@ let port = process.env.PORT || 8000;
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
   next();
 });
 
